2d5cfba9b03a5d4fe101d1179f4683c17307d403
Cosinusjustafonction <64425437+Cosinusjustafonction@users.noreply.github.com>
2023-08-20 17:51:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
+
 import time
 
 app = Flask(__name__)
-CORS(app)
+
 
 # Load existing email data from the file
 email_list = []
